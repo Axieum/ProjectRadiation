40a3b938310ca8db1bddbbf9da90ae42bf6ada11
Axieum <axieum@users.noreply.github.com>
2017-07-10 21:35:10 +1000

    Forge (1.11.2-13.20.1.2393) installed.

---
+++ src/main/java/me/axieum/mcmod/projectradiation/ProjectRadiation.java
@@ -0,0 +1,34 @@
+package me.axieum.mcmod.projectradiation;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ProjectRadiation.MODID, version = ProjectRadiation.VERSION)
+public class ProjectRadiation
+{
+	
+    public static final String MODID = "projectradiation";
+    public static final String VERSION = "1.0.0";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        //
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        //
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        //
+    }
+    
+}
