22dc72972f10d3d34f92eca7800daa464c222f40
Axieum <axieum@users.noreply.github.com>
2017-07-10 22:17:39 +1000

    Removed over exaggerated config comments.

--- src/main/java/me/axieum/mcmod/projectradiation/Config.java
+++ src/main/java/me/axieum/mcmod/projectradiation/Config.java
@@ -17,9 +17,6 @@ public class Config
 		// [Create] and load the file
 		Configuration config = new Configuration(new File(References.CONFIG_FILE));
 		config.load();
-		
-		// Categories
-		config.addCustomCategoryComment(References.CONFIG_CATEGORY_ENABLES, "Enable/Disable an aspect of the mod.");
 
 		// Initialise config options
 		ACHIEVEMENTS = config.get(References.CONFIG_CATEGORY_ENABLES, "Achievements", true).getBoolean();
